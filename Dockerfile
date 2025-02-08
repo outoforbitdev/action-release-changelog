71ee28dac6f30bc0bdd93d0db3d5ec60c6bfa9ad
Jay Mirecki <jarett.mirecki@gmail.com>
2025-02-08 11:56:47 -0600

    ci: use custom docker test script

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.12.9-bookworm
+
+RUN python -m pip install PyGithub
+COPY src /src
+RUN chmod +x /src
+RUN sh /src/tests.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
--- src/tests.sh
+++ src/tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 python --version | head -n 1 | grep 3.12.9
-if [ -e create_release.py ]; then
+if [ -e /src/create_release.py ]; then
     echo "create_release.py exists"
 else
     exit 1
